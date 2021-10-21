8c28256e54ac480d811e977acd227bedd432ad38
juanesosorio <Juanchis_1989@hotmail.com>
2021-10-21 14:45:49 -0500

    prueba

--- backend/script/rutas.js
+++ backend/script/rutas.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const router = express.Router();
-
-let { consulta, eliminar, actualizar, crear } = require("./consultar");
-
-router.get("/consultarclientes", consulta);
-router.get("/consultartickets", consulta);
-
-router.post("/agregarusuario", crear);
-router.post("/agregarticket", crear);
-
-router.delete("/eliminarticket", eliminar);
-router.put("/actualizarticket", actualizar);
+let {
+  consultarusuarios,
+  consultartickets,
+  loguin,
+  consultarunticket,
+  crearusuario,
+  crearticket,
+  actualizarticket,
+  cerrarticket,
+} = require("./consultar");
+router.get("/consultarusuarios", consultarusuarios);
+router.get("/consultartickets", consultartickets);
+router.post("/loguin", loguin);
+router.post("/consultarunticket", consultarunticket);
+router.post("/agregarusuario", crearusuario);
+router.post("/agregarticket", crearticket);
+router.put("/actualizarticket", actualizarticket);
+router.put("/cerrarticket", cerrarticket);
 
 module.exports = router;
