1282b5e57acfd27900f3d608d833c2fa192c727c
juanesosorio <Juanchis_1989@hotmail.com>
2021-10-23 20:26:49 -0500

    fetch listo

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = require("./script/rutas");
 const app = express();
+const cors = require("cors");
 
+app.set("port", process.env.PORT || 3000);
+app.use(cors());
 app.use(express.json());
 app.use(router);
-app.set("port", process.env.PORT || 3000);
 
 app.listen(app.get("port"), () => {
   console.log("server on por ", app.get("port"));
 });
+/* vane.vrb@gmail.com */
