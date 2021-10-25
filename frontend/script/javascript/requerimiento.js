93f4a2d3e5f1393c63ab51ef26a11a5bcbfc520d
MichaelSCR <stvn.prz2002@gmail.com>
2021-10-25 11:12:12 -0500

    Envio de solicitud

--- frontend/script/javascript/requerimiento.js
+++ frontend/script/javascript/requerimiento.js
@@ -1,3 +1,38 @@
 import Local from "./localstorage.js";
 let nombre = Local.getlocalstorage().nombre;
-console.log(nombre);
+let usuario = Local.getlocalstorage().usuario;
+let boton = document.getElementById("enviar_solicitud");
+let titulo = document.getElementById("titulo_nombre");
+//value para input; textContent para spam (coloca texto y se puede dentro de otra etiqueta);
+titulo.textContent="Bienvenido "+nombre;
+diccionario_ticket=boton.addEventListener("click", function () {
+    let solicitud =document.getElementById("dato_solicitud").value;
+    dic_ticket=crear_solicitud(solicitud);;
+    return dic_ticket;
+  });
+
+function crear_solicitud(solicitud){
+    let ticket={
+        usuario:usuario,
+        solicitud:solicitud,
+        fecha_inicio_requerimiento:fecha()};
+    return ticket;
+}
+
+function fecha(){
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+  dd = '0' + dd;
+}
+
+if (mm < 10) {
+  mm = '0' + mm;
+}
+
+today = yyyy + '/' + mm + '/' + dd;
+return today;
+}
